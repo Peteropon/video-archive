bc074b6d489e6acdcfdf61a9089e1827cc432d9c
Petros Tsavdaridis <peteropon@gmail.com>
2020-12-18 21:52:43 +0100

    Changed footer, deleted unnecessary script.

--- pages/index.js
+++ pages/index.js
@@ -51,14 +51,11 @@ export default function Home({ data }) {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>
+          Made by Petros <br /> For educational, demonstrative and showing-off
+          puproses only <br />
+          &copy; PT 2020
+        </p>
       </footer>
     </div>
   );
