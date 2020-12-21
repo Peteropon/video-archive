b434a7b35cdd147440af0f2c56fe5a2e7b4fa8d2
Petros Tsavdaridis <peteropon@gmail.com>
2020-12-21 16:44:08 +0100

    Worked on CSS.

--- pages/bands/[band].js
+++ pages/bands/[band].js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import ReactPlayer from "react-player/youtube";
 import { getBandData, getPaths } from "../../lib/videos";
 import Layout from "../../components/layout";
+import styles from "../../styles/Band.module.css";
 
 export async function getStaticProps({ params }) {
   const band = getBandData(params);
@@ -26,15 +27,21 @@ export default function Band({ band }) {
       <Head>
         <title>{band.bandName}</title>
       </Head>
-      <header>{band.bandName}</header>
-      {band.videos.map((video, key) => (
-        <ReactPlayer
-          key={key}
-          light={true}
-          controls={true}
-          url={video.url}
-        ></ReactPlayer>
-      ))}
+      <header className={styles.header}>{band.bandName}</header>
+      <ul className={styles.list}>
+        {band.videos.map((video, key) => (
+          <li>
+            <ReactPlayer
+              key={key}
+              light={true}
+              controls={true}
+              url={video.url}
+              width="100%"
+              height="100%"
+            ></ReactPlayer>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
