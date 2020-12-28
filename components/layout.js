2c912d2469a265b7e63e406d72304a57b6b64d42
Petros Tsavdaridis <peteropon@gmail.com>
2020-12-28 12:30:44 +0100

    Refactored layout.

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,17 @@
 import Head from "next/head";
-import Link from "next/link";
 import styles from "./layout.module.css";
 
-export const siteTitle = "Petros Video Archive";
+const siteTitle = "Petros Video Archive";
 
-export default function Layout({ children, home }) {
+export default function Layout({ children }) {
   return (
     <div className={styles.container}>
       <Head>
+        <title>{siteTitle}</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Archive of my videos using Next.js" />
         <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <main>{children}</main>
       <footer className={styles.footer}>
