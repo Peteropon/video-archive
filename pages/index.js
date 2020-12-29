91c46e65a85b5afdfb11a08d4a6132f83f54bcdf
Petros Tsavdaridis <peteropon@gmail.com>
2020-12-29 16:31:50 +0100

    Deleted page transitions.

--- pages/index.js
+++ pages/index.js
@@ -13,22 +13,6 @@ export async function getStaticProps() {
   };
 }
 
-const postVariants = {
-  initial: { scale: 0.86, y: 50, opacity: 0 },
-  enter: {
-    scale: 1,
-    y: 0,
-    opacity: 1,
-    transition: { duration: 1, ease: [0.48, 0.15, 0.25, 0.96] },
-  },
-  exit: {
-    scale: 0.8,
-    y: 30,
-    opacity: 0,
-    transition: { duration: 0.15, ease: [0.48, 0.15, 0.25, 0.96] },
-  },
-};
-
 const headerVariants = {
   hidden: { scale: 0.8, opacity: 0 },
   visible: {
@@ -57,39 +41,27 @@ export default function Home({ data }) {
           <h1 className={styles.title}>Welcome to my video archives!</h1>
         </motion.div>
 
-        <motion.div
-          layout
-          initial="initial"
-          animate="enter"
-          exit="exit"
-          variants={{
-            exit: { transition: { ease: "easeOut", staggerChildren: 0.1 } },
-          }}
-        >
-          <ul className={styles.grid}>
-            {data.map(({ path, bandName, videos }, key) => (
-              <motion.li
-                scroll={false}
-                key={key}
-                className={styles.card}
-                variants={postVariants}
-                whileHover={{
-                  position: "relative",
-                  zIndex: 1,
-                  scale: 1.1,
-                  transition: { duration: 0.4 },
-                }}
-              >
-                <Link scroll={false} href={`/bands/${path}`}>
-                  <a>
-                    <h3>{bandName} &rarr;</h3>
-                    <p>{videos.length} videos</p>
-                  </a>
-                </Link>
-              </motion.li>
-            ))}
-          </ul>
-        </motion.div>
+        <ul className={styles.grid}>
+          {data.map(({ path, bandName, videos }, key) => (
+            <motion.li
+              key={key}
+              className={styles.card}
+              whileHover={{
+                position: "relative",
+                zIndex: 1,
+                scale: 1.1,
+                transition: { duration: 0.4 },
+              }}
+            >
+              <Link scroll={false} href={`/bands/${path}`}>
+                <a>
+                  <h3>{bandName} &rarr;</h3>
+                  <p>{videos.length} videos</p>
+                </a>
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
       </main>
     </Layout>
   );
