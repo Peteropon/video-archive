35d4f1f7a7e873be935112fbf75d8981bc0896f5
Petros Tsavdaridis <peteropon@gmail.com>
2020-12-31 15:50:04 +0100

    Added an animation for header background.

--- components/header.js
+++ components/header.js
@@ -2,23 +2,41 @@ import styles from "./header.module.css";
 import { motion } from "framer-motion";
 
 const headerVariants = {
-  hidden: { scale: 0.8, opacity: 0 },
+  hidden: { y: "-100%", scale: 1, opacity: 0 },
+  visible: {
+    y: "0%",
+    scale: 1,
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 1,
+    },
+  },
+};
+
+const titleVariants = {
+  hidden: { scale: 0.6, opacity: 0 },
   visible: {
     scale: 1,
     opacity: 1,
     transition: {
       ease: "easeOut",
-      delay: 0.3,
+      delay: 0.8,
       duration: 1,
     },
   },
 };
 export default function Header() {
   return (
-    <header className={styles.titleWrapper}>
-      <motion.div initial="hidden" animate="visible" variants={headerVariants}>
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={headerVariants}
+      className={styles.titleWrapper}
+    >
+      <motion.div initial="hidden" animate="visible" variants={titleVariants}>
         <h1 className={styles.title}>Petros video archives</h1>
       </motion.div>
-    </header>
+    </motion.div>
   );
 }
