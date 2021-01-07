ce3751409539db905ea2feb06fc5fdd11628dba2
Petros Tsavdaridis <peteropon@gmail.com>
2021-01-07 14:40:29 +0100

    Moved state comp to band so that only one selected accordion is open at a time.

--- components/videoAccordion.js
+++ components/videoAccordion.js
@@ -3,7 +3,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { Player } from "./player";
 import styles from "./videoAccordion.module.css";
 
-const Accordion = ({ title, url, expanded, setExpanded }) => {
+export const VideoAccordion = ({ title, url, expanded, setExpanded }) => {
   const isOpen = title === expanded;
 
   return (
@@ -42,18 +42,5 @@ const Accordion = ({ title, url, expanded, setExpanded }) => {
         )}
       </AnimatePresence>
     </motion.div>
-  );
-};
-
-export const VideoAccordion = ({ video }) => {
-  const [expanded, setExpanded] = useState(0);
-
-  return (
-    <Accordion
-      title={video.title}
-      url={video.url}
-      expanded={expanded}
-      setExpanded={setExpanded}
-    />
   );
 };
